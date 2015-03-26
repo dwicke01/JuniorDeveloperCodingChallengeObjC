9f0f4f965a8b9031d0ad77bbe0d4206a1ff35255
dwicke01 <dwicke01@students.poly.edu>
2015-03-26 02:15:09 -0400

    Initial Commit

---
+++ JuniorDeveloperCodingChallengeObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JuniorDeveloperCodingChallengeObjC
+//
+//  Created by Daniel Wickes on 3/26/15.
+//  Copyright (c) 2015 danielwickes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JuniorDeveloperCodingChallengeObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JuniorDeveloperCodingChallengeObjC
+//
+//  Created by Daniel Wickes on 3/26/15.
+//  Copyright (c) 2015 danielwickes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
